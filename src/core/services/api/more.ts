5026396510e588eb1f99e505f72536c8361bd6d9
Mohammad_jalali <167128241+Mohmmad-hosein@users.noreply.github.com>
2024-11-22 19:50:00 +0330

    .

--- src/core/services/api/more.ts
+++ src/core/services/api/more.ts
@@ -9,7 +9,7 @@ export const ReportComment = async (data: {
 }) => {
   try {
     const res = await axios.post("https://673d05514db5a341d833bf75.mockapi.io/report/Comment-Report", data);
-    return res;
+    return res.data;
   } catch (error) {
     console.log(error, "Error posting");
     return null;
